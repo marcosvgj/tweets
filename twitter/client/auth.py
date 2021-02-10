33d4d122c40eb512bbbf2236c8efc1051f7bd2c5
marcosvgj <marcos.guimaraesnetwork@gmail.com>
2021-02-09 23:34:58 -0300

    Add auth module of twitter / tweepy SDK

---
+++ twitter/client/auth.py
@@ -0,0 +1,29 @@
+import os
+import logging
+import pathlib
+import yaml
+from typing import Optional, Text, DefaultDict
+
+__all__ = ["CredentialsHandler"]
+
+
+class CredentialsHandler:
+    """
+    Interface of OAuth 1a (application-user) credentials recovery.
+    """
+
+    @staticmethod
+    def read_credentials_from_file(
+        path="resources/credentials.yaml", key="twitter_app"
+    ):
+        try:
+            with open(pathlib.Path(__file__).parent.parent / path) as cfg:
+                return yaml.load(cfg, Loader=yaml.FullLoader).get(key)
+        except Exception as error:
+            logging.error(
+                f"Error in load configuration file inside resources folder. Details: {error}"
+            )
+
+    @staticmethod
+    def read_credentails_from_vault():
+        pass
