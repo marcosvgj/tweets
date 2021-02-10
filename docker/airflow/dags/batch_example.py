ff3ef1ba78aab077fbbc15cef7fc7baac2a37277
marcosvgj <marcos.guimaraesnetwork@gmail.com>
2021-02-10 18:26:28 -0300

    Add dag of example

--- docker/airflow/dags/batch_example.py
+++ docker/airflow/dags/batch_example.py
@@ -2,11 +2,7 @@ from datetime import timedelta, datetime
 from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.operators.python_operator import PythonOperator
-from pyspark.sql import SparkSession
-from twitter.client import TwitterClient
-from twitter.client.auth import CredentialsHandler
-from twitter.scrapers.batch import Search
-
+from twitter.sink.spark.batch import Pipeline
 
 default_args = {
     "owner": "Doctor Who",
@@ -21,19 +17,7 @@ default_args = {
 
 
 def run():
-    sc = (
-        SparkSession.builder.master("local[*]")
-        .appName("pipeline_batch_example")
-        .getOrCreate()
-    )
-    spark = sc.get_session()
-    twd_credentials = CredentialsHandler.read_credentials_from_file()
-    client = TwitterClient(**twd_credentials)
-    data = Search.query(client=client, hashtags=["COVID19"])
-
-    spark.createDataFrame(data).write.mode("append").option(
-        "path", "hdfs://hdfs:9000/tweets/covid"
-    ).format("parquet").saveAsTable("covid_tweets")
+    Pipeline.dataset(save=True, hashtags=["COVID19"])
 
 
 dag = DAG(
@@ -42,6 +26,7 @@ dag = DAG(
     description="Pipeline Batch - Twitter data scraper",
     schedule_interval=timedelta(minutes=15),
 )
+
 
 task = PythonOperator(
     dag=dag,
