8cdea7847eb26f0472490c6789619e4189d8bbc8
marcosvgj <marcos.guimaraesnetwork@gmail.com>
2021-02-14 01:46:29 -0300

    Fix credentials key type in credentials.yaml

--- credentials_setup/setup.py
+++ credentials_setup/setup.py
@@ -3,6 +3,7 @@ import yaml
 
 credentials_path = os.environ.get("TWEETS_CREDENTIAL_PATH")
 
+
 def workflow():
     access_token = input("Twitter Developer Access Token: ")
     access_token_secret = input("Twitter Developer Access Token Secret: ")
@@ -11,7 +12,7 @@ def workflow():
     with open(credentials_path, "w") as filepath:
         data = dict(
             {
-                "twitter_api": {
+                "twitter_app": {
                     "access_token": access_token,
                     "access_token_secret": access_token_secret,
                     "consumer_key": consumer_key,
@@ -21,6 +22,7 @@ def workflow():
         )
 
         yaml.dump(data, filepath)
+
 
 if __name__ == "__main__":
     workflow()
